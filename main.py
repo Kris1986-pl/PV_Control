ad13542d3dfd3e398938b77f419737e52319354e
Kris1986-pl <krzysztof.kinel86@gmail.com>
2023-07-05 20:38:12 +0200

    customize main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from decouple import config
 import json
 from requests import get, Session
@@ -7,18 +6,23 @@ from urllib.parse import urljoin
 import logging
 
 # Load configuration values from .env file or environment variables
+# Server configuration - URL and Access token configuration of the Supla Cloud service (https://cloud.supla.org/)
 SERVER = config('SERVER')
 PERSONAL_ACCESS_TOKEN = config('PERSONAL_ACCESS_TOKEN')
+# Fronius service-related configuration
 ID = config('ID')
-PV_URL = config('PV_URL')
+# PV_URL for Fronius service - URL for accessing the Fronius API
+PV_URL = config('PV_URL') + "solar_api/v1/GetPowerFlowRealtimeData.fcgi"
 
 # Set up base URL and session for API requests
 base_url = f'https://{SERVER}/api/'
 session = Session()
 session.headers['Authorization'] = f'Bearer {PERSONAL_ACCESS_TOKEN}'
+log_file = 'app.log'
+counter = 0
 
-# Konfiguracja logowania
-logging.basicConfig(filename='/var/www/html/app.log', level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+# Configure logging
+logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 logger = logging.getLogger(__name__)
 
 def fetch_device_state(id_device):
@@ -43,10 +47,10 @@ def fetch_pv_value(url):
 
 
 while True:
+    counter += 1
     logger.info("*"*20)
-    logger.info(datetime.now().strftime("%H:%M:%S"))
-    pv_value = fetch_pv_value(PV_URL)
-    logger.info(f'Current power PV: {pv_value}')
+    pv_value = fetch_pv_value(PV_URL)  # Log separator
+    logger.info(f'Current power PV: {pv_value}')  # Log current PV power value
 
     # Check if PV value exceeds threshold and device state needs to be updated
     if pv_value > 2999 and not fetch_device_state(ID):
@@ -56,5 +60,9 @@ while True:
         update_device_parameters(ID, {"action": "TURN_OFF"})
         logger.info("Device is turn ON" if fetch_device_state(ID) else "Device is turn OFF")
     else:
-        logger.info("Didn't change stata")
-    sleep(3)
+        logger.info("No state change")  # Log when no state change occurs
+    sleep(600)  # Sleep for 10 minutes
+    if counter == 144:
+        counter = 0
+        with open(log_file, 'w') as file:
+            file.write('')  # Clear log file at the end of the day
