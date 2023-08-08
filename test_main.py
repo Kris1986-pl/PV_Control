9428f0cb0e480007c1b75f3acf8e17cca541a3a7
Kris1986-pl <krzysztof.kinel86@gmail.com>
2023-08-08 21:44:48 +0200

    added new tests

--- test_main.py
+++ test_main.py
@@ -1,7 +1,12 @@
-from main import fetch_device_state
+from main import fetch_device_state, env_is_dev, fetch_pv_value
 from decouple import config
 
 ID = config('ID')
+
+
+def test_fetch_pv_value_is_positive_number():
+    pv_value = fetch_pv_value()
+    assert pv_value > 0
 
 
 def test_fetch_device_state_valid_id():
@@ -14,3 +19,7 @@ def test_fetch_device_state_invalid_id():
     id_device = 1
     expected_state = -1
     assert fetch_device_state(id_device) == expected_state
+
+
+def test_env_is_dev():
+    assert env_is_dev() is True
